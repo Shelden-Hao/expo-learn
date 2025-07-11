afc2684c010b5ca0a5854eba7f248d4562d10c6b
hxg <2483386026@qq.com>
2025-07-11 15:03:30 +0800

    feat: init

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <Button title="Click me" onPress={() => setCount(count + 1)} />
     </View>
   );
 }
