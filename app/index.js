5f449a6a226d8d7add8d40bf38471d5b1c23eee3
hxg <2483386026@qq.com>
2025-07-15 10:15:18 +0800

    feat: 路由Link使用

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>这是一个新的入口！</Text>
+      <Text style={styles.title}>这里是首页</Text>
+      <Link href="/details" style={styles.link}>
+        跳转到详情页（Link）
+      </Link>
+      <Link href="/details" asChild>
+        <TouchableOpacity>
+          <Text style={styles.buttonText}>跳转到详情页（Link + asChild）</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
@@ -16,9 +25,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: {
-    fontSize: 50,
-    width: 200,
+    fontSize: 40,
     fontWeight: "bold",
     color: "#e29447",
+  },
+  link: {
+    marginTop: 20,
+    fontSize: 20,
+    color: "#1f99b0",
+  },
+  buttonText: {
+    marginTop: 20,
+    fontSize: 20,
+    color: "#ff7f6f",
   },
 });
