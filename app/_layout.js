7e9e516ea3239628af264df595424c8c9391e378
hxg <2483386026@qq.com>
2025-07-15 18:07:24 +0800

    feat: 插槽测试

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,26 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerTitleAlign: "center", // 安卓标题栏居中
-        animation: "slide_from_right", // 安卓使用左右切屏
-      }}
-    />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>App header</Text>
+      <Slot />
+      <Text style={styles.footer}>© 版权所有</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    fontSize: 24,
+    textAlign: "center",
+  },
+  footer: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
